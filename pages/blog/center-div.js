3826a905e92fa36d9b6f756ae15764299575ab9d
Edmond chen <chen.edmond13@gmail.com>
2021-07-09 12:26:57 -0700

    added portfolio pages

---
+++ pages/blog/center-div.js
@@ -0,0 +1,98 @@
+import Link from 'next/link';
+import Image from 'next/image';
+import logo from '../../public/logo/EC.png';
+import { FaLinkedin, FaGithub} from 'react-icons/fa';
+import div from '../../public/projects/Div.png';
+
+const ConstDiv = () => {
+
+
+    return (
+        <div>
+            <header className="flex justify-between h-auto w-full mt-4">
+                <div className="ml-2">
+                    <Link href="/">
+                        <Image src={logo} width={127} height={80} alt="portfolio logo" />
+                    </Link>
+                </div>
+                <nav className="flex">
+                    <div className="invisible flex flex-col md:visible md:flex-row md:space-x-6 md:m-auto md:mr-10">
+                        <Link href="/#projects">
+                            <a className="hover:text-red-500">Projects</a>
+                        </Link>
+
+                        <Link href="/#experience">
+                            <a className="hover:text-red-500">Experience</a>
+                        </Link>
+                    </div>
+                    <div className="flex m-auto space-x-4 mr-2 md:mr-6">
+                        <a href="https://www.linkedin.com/in/edmond-chen-5b0b111a5/">
+                            <FaLinkedin className="text-3xl" />
+                        </a>
+                        <a href="https://github.com/echen12">
+                            <FaGithub className="text-3xl" />
+                        </a>
+                    </div>
+                </nav>
+            </header>
+
+            <div>
+                <div className="flex justify-center m-4">
+                    <Image src={div} alt="blog thumbnail" />
+                </div>
+
+                <div>
+                    <div className="text-center text-xl font-bold">
+                        <h1>Let's Center A Div!</h1>
+                    </div>
+
+                    <div className="flex justify-center">
+
+                        <div className="md:w-9/12 m-2">
+
+                            <p>Centering a div has become a meme among developers because something so seemingly simple can quickly turn into a frustrating task if you aren't familiar with how CSS works.
+                            </p>
+                            <p>
+                                Let&apos;s start with the template below. The goal is to center the child box inside its parent.
+                            </p>
+
+                            <br />
+
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                            </p>
+
+
+                        </div>
+
+
+                    </div>
+
+                </div>
+            </div>
+
+            <div className="border-t-2 mt-8">
+
+                <div className="flex justify-between mt-8 sm:mt-20 ml-4 mb-4">
+                    <div className="w-7/12">
+                        <p className="text-sm">Powered by dank memes and dank dreams. :')</p>
+                    </div>
+                    <div className="flex space-x-4 sm:space-x-10 text-3xl mr-4">
+                        <a href="https://www.linkedin.com/in/edmond-chen-5b0b111a5/">
+                            <FaLinkedin className="text-3xl" />
+                        </a>
+                        <a href="https://github.com/echen12">
+                            <FaGithub className="text-3xl" />
+                        </a>
+                    </div>
+                </div>
+
+            </div>
+
+
+
+        </div>
+    )
+}
+
+export default ConstDiv;
---
+++ pages/projects/todo-list.js
@@ -0,0 +1,108 @@
+import Link from 'next/link'
+import Image from 'next/image'
+import logo from '../../public/logo/EC.png'
+import todo from '../../public/projects/todo-list-screenshot.png'
+import { FaLinkedin, FaGithub, FaArrowRight, FaReact, FaNode } from 'react-icons/fa'
+import { DiMongodb } from "react-icons/di";
+import Footer from '../../components/Footer'
+
+
+const FleetCare = () => {
+    return (
+        <div>
+
+            <header className="flex justify-between h-auto w-full mt-4">
+                <div className="ml-2">
+
+                    <Link href="/">
+                        <Image src={logo} width={127} height={80} alt="portfolio logo" />
+                    </Link>
+
+                </div>
+                <nav className="flex">
+                    <div className="invisible flex flex-col md:visible md:flex-row md:space-x-6 md:m-auto md:mr-10">
+                        <Link href="/#projects">
+                            <a className="hover:text-red-500">Projects</a>
+                        </Link>
+
+                        <Link href="/#experience">
+                            <a className="hover:text-red-500">Experience</a>
+                        </Link>
+
+                    </div>
+                    <div className="flex m-auto space-x-4 mr-2 md:mr-6">
+                        <a href="https://www.linkedin.com/in/edmond-chen-5b0b111a5/">
+                            <FaLinkedin className="text-3xl" />
+                        </a>
+                        <a href="https://github.com/echen12">
+                            <FaGithub className="text-3xl" />
+                        </a>
+                    </div>
+                </nav>
+            </header>
+
+            <div>
+                <div className="flex flex-col m-4">
+                    <div className="lg:flex lg:justify-evenly border-b-2 m-2">
+
+                        <div className="m-2 lg:w-7/12">
+                            <h1 className="text-5xl m-2 pb-2 border-b-2">ToDo-List</h1>
+                            <div className="space-y-4 text-xl m-4">
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                                </p>
+                                <p className="text-sm">
+                                    *This project is hosted on Heroku. Please give it a few seconds to load! 
+                                </p>
+
+                                <div className="space-y-2">
+                                    <a className="flex w-full justify-start text-blue-600" href="https://tudu-list.herokuapp.com/">
+                                        <p>View Site</p>
+                                        <FaArrowRight className="mt-1 ml-2" />
+                                    </a>
+
+                                    <a className="flex w-full justify-start text-blue-600" href="https://github.com/echen12/ToDo-List">
+                                        <p>View Code</p>
+                                        <FaArrowRight className="mt-1 ml-2" />
+                                    </a>
+                                </div>
+
+                            </div>
+                        </div>
+
+                        <div className="flex flex-col m-2 lg:w-1/4 space-y-4" >
+
+                            <div className="m-2">
+                                <div className="space-y-2">
+                                    <h1 className="text-4xl">Highlights</h1>
+                                    <ul className="md:text-2xl">
+                                        <li>- MERN Stack</li>
+                                    </ul>
+                                </div>
+                            </div>
+
+
+                            <div className="flex justify-evenly pb-2">
+                                <FaReact className="text-5xl" />
+                                <FaNode className="text-5xl" />
+                                <DiMongodb className="text-5xl" />
+                            </div>
+
+                        </div>
+
+                    </div>
+
+                    <div className="h-0 md:h-auto flex flex-col mt-4">
+                        <Image src={todo} className="invisible md:visible" alt="project thumbnail" />
+                    </div>
+                </div>
+
+            </div>
+
+            <Footer />
+
+        </div>
+    )
+}
+
+export default FleetCare;
