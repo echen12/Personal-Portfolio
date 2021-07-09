710c73fceea40d8a217c2138ac3762257b338b76
Edmond chen <chen.edmond13@gmail.com>
2021-07-09 12:03:26 -0700

    added intro to home

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 import NavBar from "../components/NavBar";
+import Introduction from "../components/Introduction";
 
 const Home = () => {
   return (
     <div>
       <NavBar />
+      <Introduction />
     </div>
   )
 }
