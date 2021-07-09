7883bce615bacac0d4c7055e572a0b25f5357c9c
Edmond chen <chen.edmond13@gmail.com>
2021-07-09 12:18:33 -0700

    updated apostrophe

--- components/Footer.js
+++ components/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
 
             <div className="flex justify-between mt-8 sm:mt-20 ml-4 mb-4">
                 <div className="w-7/12">
-                    <p className="text-sm">Powered by dank memes and dank dreams. :')</p>
+                    <p className="text-sm">Powered by dank memes and dank dreams. :&apos;)</p>
                 </div>
                 <div className="flex space-x-4 sm:space-x-10 text-3xl mr-4">
                     <a href="https://www.linkedin.com/in/edmond-chen-5b0b111a5/">
