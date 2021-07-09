47e2dcacc284eb26ac3baa6e8d0f5048e3b96996
Edmond chen <chen.edmond13@gmail.com>
2021-07-09 12:10:44 -0700

    added project to home

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,13 @@
 import NavBar from "../components/NavBar";
 import Introduction from "../components/Introduction";
+import Projects from "../components/Projects";
 
 const Home = () => {
   return (
     <div>
       <NavBar />
       <Introduction />
+      <Projects />
     </div>
   )
 }
