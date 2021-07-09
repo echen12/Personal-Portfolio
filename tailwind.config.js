2c6d65cd8de20b4dd0138517e0ad469c7f526137
Edmond chen <chen.edmond13@gmail.com>
2021-07-09 12:08:42 -0700

    updated color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'palette-aqua': '#87A7B3',
+        'custom-orange': '#ff9f1c',
+        'custom-teal': '#d0f4de',
+        'custom-yellow': '#fff3b0',
+        'custom-purple': '#8a2be2',
+        'palette-brown': '#D8B384',
+        'palette-purple': '#C490E4',
+      },
+    },
   },
   variants: {
     extend: {},
